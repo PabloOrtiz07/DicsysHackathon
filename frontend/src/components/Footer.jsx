87b8685328245f40bb0faec36c392462257e4958
Brian Pumarica <brianpumarica@gmail.com>
2023-08-24 19:35:56 -0300

    edit footer text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     return (
         <div className='fixed bottom-0 bg-slate-800 text-white w-full overflow-x-hidden'>
             <div style={{ height: '8vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <p>FOOTER.</p>
+                <p>Dicsys 2023 - Derechos Reservados.</p>
             </div>
         </div>
     )

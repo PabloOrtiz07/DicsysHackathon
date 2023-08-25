f6a603141a93ca8b7a0ee6c76117e7155db989af
Brian Pumarica <brianpumarica@gmail.com>
2023-08-25 19:34:00 -0300

    add backend folder -> WebApplicationHackathon

---
+++ WebApplicationHackathon/WebApplicationHackathon/Controllers/IngresanteController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationHackathon.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Diagnostics.Metrics;
+
+namespace WebApplicationHackathon.Controllers
+{
+    public class IngresanteController : Controller
+    {
+        private readonly DbdicsysContext _DBContext;
+
+        public IngresanteController(DbdicsysContext context)
+        {
+            _DBContext = context;
+        }
+
+
+        public IActionResult Index(int? selectedAge, string selectedCountry, bool? younger, bool? older, int? selectedExperience)
+        {
+            List<Ingresante> _ingresante = _DBContext.Ingresantes.ToList();
+            var filteredPeople = _ingresante;
+
+            if (selectedAge.HasValue)
+            {
+                if (younger == true)
+                {
+                    filteredPeople = filteredPeople.Where(ingresante => ingresante.Edad > selectedAge.Value).ToList();
+                }
+                else if (older == true)
+                {
+                    filteredPeople = filteredPeople.Where(ingresante => ingresante.Edad < selectedAge.Value).ToList();
+                }
+                else
+                {
+                    filteredPeople = filteredPeople.Where(ingresante => ingresante.Edad == selectedAge.Value).ToList();
+
+                }
+            }
+
+            if (!string.IsNullOrEmpty(selectedCountry))
+            {
+                filteredPeople = filteredPeople.Where(ingresante => ingresante.Nacionalidad == selectedCountry).ToList();
+            }
+
+          
+
+            if (selectedExperience.HasValue)
+            {
+                filteredPeople = filteredPeople.Where(ingresante => ingresante.AniosExperiencia > selectedExperience.Value).ToList();
+            }
+
+            return View(filteredPeople);
+        }
+
+        [HttpGet]
+        public IActionResult Ingresante_Registro()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ingresante_Registro(Ingresante ingresante)
+        {
+            // No es necesario asignar un nuevo ID manualmente
+
+            _DBContext.Ingresantes.Add(ingresante);
+            _DBContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplicationHackathon/WebApplicationHackathon/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplicationHackathon.Models;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Dni { get; set; }
+
+    public string? Contrasena { get; set; }
+}
